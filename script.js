9f447e760d00ccc4fe3480e1513bc323b61302b5
500apps-RahulVangari <85161282+500apps-RahulVangari@users.noreply.github.com>
2021-06-02 20:25:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*let js = 'amazing';
+let first = "rahul";
+f = true;
+console.log(40 + 8 + 23 - 10);
+console.log(first);
+console.log(typeof f);
+f = 23;
+console.log(typeof f);*/
+// const m1 = 78, m2 = 1.69;
+// const h1 = 1.69, h2 = 1.95;
+// const bmi1 = m1 / h1 ** 2, bmi2 = m2 / h2 ** 2;
+// console.log(bmi1, bmi2, bmi1 > bmi2);
+// const y = "1991";
+// console.log(typeof Number(y), y);
+// console.log(y + 18);
+// const h = 'rahul'
+// console.log(Number(h));
+// console.log('Iam ' + 23 + ' years old');
+// console.log('23' + 'hi' + 10 + '3' + 'hi');
+
+// let n = "1" + 1;//n=11 
+// n = n - 1;
+// console.log(n);
+const age = '18'
+if (age === 18) console.log("Not equal");
+if (age == 18) console.log("equal");
+// const f = Number(prompt("Number"));
+// console.log(f);
+// console.log(typeof f);
+const drivinglicense = true;
+const vision = false;
+console.log("Can sarah drive with a license and without vision");
+if (drivinglicense && vision)
+    console.log("Yes, can drive");
+else
+    console.log("No, that may lead to a disaster");
